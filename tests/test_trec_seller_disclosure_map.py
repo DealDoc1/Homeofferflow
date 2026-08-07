88bab7238a93decc04b12ddb4e1585b2bd9064db
DealDoc1 <andrewchri@gmail.com>
2026-08-07 16:03:40 -0500

    Update item-row regression for thirty-three reviewed responses

--- tests/test_trec_seller_disclosure_map.py
+++ tests/test_trec_seller_disclosure_map.py
@@ -120,6 +120,18 @@ class TrecSellerDisclosureMapTests(unittest.TestCase):
                 ("central_ac", 58, 390),
                 ("central_heating", 211, 390),
                 ("wall_window_ac", 391, 390),
+                ("plumbing_system", 58, 372),
+                ("septic_system", 211, 372),
+                ("public_sewer_system", 391, 372),
+                ("patio_decking", 58, 354),
+                ("outdoor_grill", 211, 354),
+                ("fences", 391, 354),
+                ("pool", 58, 336),
+                ("sauna", 211, 336),
+                ("spa", 391, 336),
+                ("pool_equipment", 58, 318),
+                ("pool_heater", 211, 318),
+                ("automatic_lawn_sprinkler_system", 391, 318),
             ),
         )
 
